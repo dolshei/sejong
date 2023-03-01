a91f4c332c234ba978f48049350d6942561616aa
MacBook <leechanghun@outlook.com>
2023-03-01 19:00:45 +0900

    단위테스트 용 로또 생성기

---
+++ src/test/java/com/prototype/sejong/util/LottoNumberGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.prototype.sejong.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoNumberGeneratorTest {
+
+    @DisplayName("로또 번호 갯수 테스트")
+    @Test
+    void lottoNumberSizeTest() {
+        // given(준비) : 어떠한 데이터가 준비 되었을 때
+
+        // when(실행) : 어떠한 함수를 실행하면
+
+        // then(검증) : 어떠한 결과가 나와야 한다.
+    }
+}
